87298fd2e1365986adb846fc42eb8a5448fa6cc4
Gerdon Mafra <gerdon_mafra@hotmail.com>
2018-08-26 02:43:48 -0300

    Implementação dos métodos create, update, delete e get by uid para o serviço task.service

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,8 +1,10 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 
 import { Task } from './models/task.model';
+import { promise } from 'protractor';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +21,30 @@ export class TaskService {
 
   private setTasks(): void {
     this.tasks = this.db.collection<Task>('/tasks');
+  }
+
+  create(task: Task): Promise<void> {
+    const uid = this.db.createId();
+    return this.tasks.doc<Task>(uid)
+      .set({
+        uid,
+        title: task.title,
+        done: false
+      });
+  }
+
+  update(task: Task): Promise<void> {
+    return this.tasks.doc<Task>(task.uid)
+      .update(task);
+  }
+
+  delete(task: Task): Promise<void> {
+    return this.tasks.doc<Task>(task.uid)
+      .delete();
+  }
+
+  get(uid: string): Observable<Task> {
+    return this.tasks.doc<Task>(uid)
+      .valueChanges();
   }
 }
